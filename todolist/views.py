a557d2218c5d3f9aad0de443658e68def91b6ff7
Matthijs <matthijs.kemp@atos.net>
2018-10-02 12:47:41 +0200

    added template and view and url for first model

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import *
 
-# Create your views here.
+
+def item_list(request):
+    items = TodoItem.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'todolist/item_items.html', {'items': items})
